f69661773e4ae917d7d4721f5d9057b4bfbc77d0
Samuka007 <samuka007@dragonos.org>
2024-10-28 17:04:22 +0800

    feat: image_stream detect

--- yolo_flask_backend/flask_backend.py
+++ yolo_flask_backend/flask_backend.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import base64
 import cv2
+import numpy as np
 
 from object import ObjectDetector
 import threading
@@ -17,7 +18,7 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
 CORS(app, origins=["http://localhost:5173"])
 detector = ObjectDetector()
-
+# get_frame = 0
 
 @app.route('/')
 def index():
@@ -62,6 +63,29 @@ def handle_video_stream(data: dict):
         cap.release()
         os.remove(temp_file_path)
 
+
+@socketio.on('image_stream')
+def image_stream(data: dict):
+    # 获取图片的 Base64 数据
+    image_data = data['image']
+
+    # 将 Base64 数据转换为 NumPy 数组
+    _header, encoded = image_data.split(';base64,')
+    data = base64.b64decode(encoded)
+    nparr = np.frombuffer(data, np.uint8)
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    # global get_frame
+    # get_frame += 1 
+    # if get_frame == 15:
+    #     # 保存当前帧到指定路径
+    #     cv2.imwrite('./test_image.jpeg', frame)
+
+    # 进行缺陷检测
+    boxes = detector.detect(frame)
+    if boxes is not None:
+        print(boxes)
+        emit('defect_result', {'defect': boxes})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
